45523b153060149f0ba466af228ea6a756c89752
Kara-Morgan <70274712+Kara-Morgan@users.noreply.github.com>
2021-02-25 10:39:15 -0500

    Some debugging

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,21 +3,15 @@ import Navitem from './Navitem';
 
 class Navbar extends Component {
     constructor(props)
-    {
-    super(props);
-    this.state={
-    'NavItemActive':''
-    }
-    }
+    {super(props);this.state={ 'NavItemActive':'' }  }
     activeitem=(x)=>
-    {
-    if(this.state.NavItemActive.length>0){
-    document.getElementById(this.state.NavItemActive).classList.remove('active');
-    }
+    {  if(this.state.NavItemActive.length>0){
+         document.getElementById(this.state.NavItemActive).classList.remove('active');
+         }
     this.setState({'NavItemId':x},()=>{
-    document.getElementById(this.state.NavItemActive).classList.add('active');
-    });
-    };
+        document.getElementById(this.state.NavItemActive).classList.add('active');
+         });
+     };
     render() {
         return (
             <nav>
